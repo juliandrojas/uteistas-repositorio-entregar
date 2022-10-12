da7ab5794948e39b6bfcb2bae8d353522b94c341
Julian D Rojas <juliandra140201@gmail.com>
2022-10-11 21:13:01 -0500

    Add the module

--- backend/app.js
+++ backend/app.js
@@ -4,11 +4,6 @@ const express = require('express');
 const colors = require('colors');
 //Guardamos en una variable la inicialización del módulo Express
 const server = express();
-//Configuramos una página de inicio
-server.get('/', function(request, response) {
-    response.send('<h1>Hola Mundo con Express & NodeJS</h1>');
-    response.end();
-})
 //Configuramos puerto y una función
 server.listen(3000, function() {
     console.log("Server on port 3000".yellow);
